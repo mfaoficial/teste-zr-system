4689baa5f71bfd28e69779bf7d42748c56303174
Matheus Ferreira Aquino <mfaoficial@gmail.com>
2023-02-03 07:34:33 -0300

    Transformar data brasileira em data internacional

--- app/Http/Controllers/PersonsController.php
+++ app/Http/Controllers/PersonsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 
@@ -31,7 +32,7 @@ class PersonsController extends Controller
                     data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                     <i class="fa fa-pencil" aria-hidden="true"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-danger deleteBtn" data-id="' . $row->id  . '">
+                    <button type="button" class="btn btn-outline-danger deleteBtn" data-id="' . $row->id . '">
                     <i class="fa fa-trash-o" aria-hidden="true"></i>
                     </button>';
                 })
@@ -40,15 +41,61 @@ class PersonsController extends Controller
         }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $validator = \Illuminate\Support\Facades\Validator::make(
+                $request->all(),
+                [
+                    'name'        => 'required',
+                    'type'        => 'required',
+                    'cpf'         => 'required_with:typeCpf',
+                    'cnpj'        => 'required_with:typeCnpj',
+                    'rg'          => 'required_with:typeCpf',
+                    'birth_date'  => 'required',
+                    'person_type' => 'required',
+                    'zip_code'    => 'required',
+                    'street'      => 'required',
+                    'number'      => 'required',
+                    'district'    => 'required',
+                    'city'        => 'required',
+                    'state'       => 'required',
+                    'phone_type'  => 'required',
+                    'phone'       => 'required',
+                ],
+                [
+                    'name.required'        => 'O campo Nome é obrigatório.',
+                    'type.required'        => 'O campo Tipo... é obrigatório.',
+                    'cpf.required_with'    => 'O campo CPF é obrigatório.',
+                    'cnpj.required_with'   => 'O campo CNPJ é obrigatório.',
+                    'rg.required_with'     => 'O campo RG é obrigatório para pessoas Físicas.',
+                    'birth_date.required'  => 'O campo Data de Nascimento é obrigatório.',
+                    'person_type.required' => 'O campo Estado Civil é obrigatório.',
+                    'zip_code.required'    => 'O campo CEP é obrigatório.',
+                    'street.required'      => 'O campo Rua é obrigatório.',
+                    'number.required'      => 'O campo Número é obrigatório.',
+                    'district.required'    => 'O campo Bairro é obrigatório.',
+                    'city.required'        => 'O campo Cidade é obrigatório.',
+                    'state.required'       => 'O campo Estado é obrigatório.',
+                    'phone_type.required'  => 'O campo Tipo de Telefone é obrigatório.',
+                    'phone.required'       => 'O campo Telefone é obrigatório.',
+                ]
+            );
+
+            if ($validator->fails()) {
+                return response()->json([
+                    "success" => false,
+                    "errors"  => $validator->errors()
+                ]);
+            } else {
+                $data = $request->all();
+                $data['birth_date'] = Carbon::createFromFormat('d/m/Y', $request['birth_date'])->format('Y-m-d');
+
+                return response()->json([
+                    'success' => Person::create($data)
+                ]);
+            }
+        }
     }
 
     /**
